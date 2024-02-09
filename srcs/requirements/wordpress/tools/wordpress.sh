d95ff2cf37824580a7a2c9b2fe93b85d1e5e31f9
hamza younsi <hamzayounsi006@gmail.com>
2024-02-09 12:51:01 -0500

    al

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,21 +1,11 @@
 #!bin/sh
-FILE_PATH=" /var/www/wordpress/wp-config.php"
-echo $DB_HOST
-echo $DATABASE_USER_NAME
-echo $DATABASE_USER_PASSWORD
-echo $DATABASE_NAME
-# while ! mariadb -h${DB_HOST} -u${DATABASE_USER_NAME} -p${DATABASE_USER_PASSWORD} ${DATABASE_NAME} &>/dev/null; 
-# do
-# 	sleep 1
-#     echo "Waiting for mariadb to start..."
-# done
-if [ ! -f "$FILE_PATH" ]; then
+if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
     chmod +x wp-cli.phar 
     mv wp-cli.phar /usr/local/bin/wp
     mkdir -p /var/www/wordpress
 
-    # chown -R root:root /var/www/wordpress
+    chown -R root:root /var/www/wordpress
     cd /var/www/wordpress/
     wp core download --allow-root --path='/var/www/wordpress'
     wp config create --dbname=$DATABASE_NAME --dbuser=$DATABASE_USER_NAME --dbpass=$DATABASE_USER_PASSWORD --dbhost=mariadb --path='/var/www/wordpress' --allow-root
